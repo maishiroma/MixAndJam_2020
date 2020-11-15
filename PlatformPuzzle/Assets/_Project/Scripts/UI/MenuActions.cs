3f2b80c944dc2a5dcb70f042487fbc21f2bb2487
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2020-11-15 00:39:03 -0800

    Created menus

---
+++ PlatformPuzzle/Assets/_Project/Scripts/UI/MenuActions.cs
@@ -0,0 +1,36 @@
+﻿namespace UI
+{
+    using UnityEngine;
+    using Base;
+
+    public class MenuActions : MonoBehaviour
+    {
+        public GameObject currentActive;
+        public AudioSource sfxPlayer;
+        public SFXWrapper buttonInteract;
+
+        public void SwitchToScreen(GameObject nextScreen)
+        {
+            currentActive.SetActive(false);
+            nextScreen.SetActive(true);
+            currentActive = nextScreen;
+
+            buttonInteract.PlaySoundClip(sfxPlayer);
+        }
+
+
+        public void GoToScene(int sceneIndex)
+        {
+            buttonInteract.PlaySoundClip(sfxPlayer);
+
+            GameManager.Instance.MoveToNextLevel(sceneIndex);
+        }
+
+        public void QuitGame()
+        {
+            buttonInteract.PlaySoundClip(sfxPlayer);
+
+            Application.Quit();
+        }
+    }
+}
